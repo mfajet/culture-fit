d8a2a55bb6a34bcdd85080666fecfe8d60061d79
KristaTheCute <kshuckerow@gmail.com>
2016-08-07 09:31:08 -0400

    Make Word Cloud

---
+++ server/WordCloudMaker.R
@@ -0,0 +1,58 @@
+#***************************************************************8
+#FROM HERE
+#HAS TO HAVE THE MARKDF from before. 
+libraru(jsonlite)
+library(wordcloud)
+library(tm)
+MARKDF <-fromJSON("test.JSON", flatten = TRUE)
+
+myCorpus <- Corpus(VectorSource(MARKDF$text))
+myCorpus <- tm_map(myCorpus, content_transformer(tolower))
+removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
+myCorpus <- tm_map(myCorpus, content_transformer(removeURL))
+
+removeNumPunct <- function(x) gsub("[^[:alpha:][:space:]]*","", x)
+myCorpus <- tm_map(myCorpus, content_transformer(removeNumPunct))
+
+myStopwords <- c(setdiff(stopwords('english'), c("r", "big")), "use", "see", "used" , "via", "amp")
+myCorpus <- tm_map(myCorpus, removeWords, myStopwords)
+myCorpus <- tm_map(myCorpus, stripWhitespace)
+
+#SAVE
+
+stemCompletion2 <- function(x, dictionary) {
+  x <- unlist(strsplit(as.character(x), " "))
+  x <- x[x != ""]
+  
+  x <- stemCompletion(x, dictionary=dictionary)
+  
+  x <- paste(x, sep="", collapse=" ")
+  
+  PlainTextDocument(stripWhitespace(x))
+  
+}
+
+
+myCorpus <- lapply(myCorpus, stemCompletion2, dictionary=myCorpusCopy)
+myCorpus <- Corpus(VectorSource(myCorpus))
+
+tdm <- TermDocumentMatrix(myCorpus,control = list(wordLengths = c(3, Inf)))
+freq.terms <- findFreqTerms(tdm, lowfreq = .05*tdm$ncol)
+term.freq <- rowSums(as.matrix(tdm))
+df <- data.frame(term = names(term.freq), freq = term.freq)
+m <- as.matrix(tdm)
+
+# calculate the frequency of words and sort it by frequency
+
+word.freq <- sort(rowSums(m), decreasing = T)
+# colors
+
+
+
+cloud <- wordcloud(words = names(word.freq), freq = word.freq, min.freq = 3,
+                   random.order = F, random.color = T, col = c("red", "royalblue1", " dark green", "grey28"))
+write(cloud, stdout())
+
+#TO HERE
+
+###########################################
