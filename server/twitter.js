e08c9b7bca649897c2890db399db8c4a77dfa64c
Mark Fajet <markfajet@gmail.com>
2016-08-07 11:58:28 -0400

    Fixed top five and made it 6

--- server/twitter.js
+++ server/twitter.js
@@ -44,32 +44,40 @@ function sendFirstTweets(res, req) {
 	var screen_name = req.body.screen_name;
 	popTweets(function(data){
   	var tweets = JSON.parse(data);
- 	var fivePop=[tweets[0],tweets[1],tweets[2],tweets[3],tweets[4]];
-  	for(var i=5;i<tweets.length;i++){
-		var obj = tweets[i];
-		var nums = obj.favorite_count + obj.retweet_count;
-		if(nums>=(tweets[0].favorite_count + tweets[0].retweet_count)){
-	  		tweets[4]=tweets[3];
-	  		tweets[3]=tweets[2];
-	  		tweets[2]=tweets[1];
-	  		tweets[1]=tweets[0];
-	  	tweets[0]=obj;
-		}else if(nums>(tweets[1].favorite_count + tweets[1].retweet_count)){
-	  		tweets[4]=tweets[3];
-	  		tweets[3]=tweets[2];
-	  		tweets[2]=tweets[1];
-	  		tweets[1]=obj;
-		}else if(nums>(tweets[2].favorite_count + tweets[2].retweet_count)){
-	  		tweets[4]=tweets[3];
-	  		tweets[3]=tweets[2];
-	  		tweets[2]=obj;
-		}else if(nums>(tweets[3].favorite_count + tweets[3].retweet_count)){
-	  		tweets[4]=tweets[3];
-	  		tweets[3]=obj;
-		}else if(nums>(tweets[4].favorite_count + tweets[4].retweet_count)){
-	  		tweets[4]=obj;
-	}
-  };
+		var fivePop=[tweets[0],tweets[1],tweets[2],tweets[3],tweets[4],tweets[5]];
+	  console.log(fivePop[0]);
+	  for(var i=6;i<tweets.length;i++){
+	    var obj = tweets[i];
+	    var nums = obj.favorite_count + obj.retweet_count;
+	    if(nums>=(fivePop[0].favorite_count + fivePop[0].retweet_count)){
+	      fivePop[5]=fivePop[4];
+	      fivePop[4]=fivePop[3];
+	      fivePop[3]=fivePop[2];
+	      fivePop[2]=fivePop[1];
+	      fivePop[1]=fivePop[0];
+	      fivePop[0]=obj;
+	    }else if(nums>(fivePop[1].favorite_count + fivePop[1].retweet_count)){
+	      fivePop[5]=fivePop[4];
+	      fivePop[4]=fivePop[3];
+	      fivePop[3]=fivePop[2];
+	      fivePop[2]=fivePop[1];
+	      fivePop[1]=obj;
+	    }else if(nums>(fivePop[2].favorite_count + fivePop[2].retweet_count)){
+	      fivePop[5]=fivePop[4];
+	      fivePop[4]=fivePop[3];
+	      fivePop[3]=fivePop[2];
+	      fivePop[2]=obj;
+	    }else if(nums>(fivePop[3].favorite_count + fivePop[3].retweet_count)){
+	      fivePop[5]=fivePop[4];
+	      fivePop[4]=fivePop[3];
+	      fivePop[3]=obj;
+	    }else if(nums>(fivePop[4].favorite_count + fivePop[4].retweet_count)){
+	      fivePop[5]=fivePop[4];
+	      fivePop[4]=obj;
+	    }else if(nums>(fivePop[5].favorite_count + fivePop[5].retweet_count)){
+	      fivePop[5]=obj;
+	    }
+	  };
   console.log(fivePop);
 	res.json(fivePop);
 }, screen_name);
