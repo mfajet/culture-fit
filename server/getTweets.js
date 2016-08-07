166a1550bc1cab9879d28676bd8a4cded9b8db46
Mark Fajet <markfajet@gmail.com>
2016-08-06 23:42:43 -0400

    Work on the way getTweets work. Less files being made. Krista's R files are included

--- server/getTweets.js
+++ server/getTweets.js
@@ -1,7 +1,9 @@
 var Twitter = require('twitter-node-client').Twitter;
+
+
 //Callback functions
     var error = function (err, response, body) {
-        console.log('ERROR [%s]', resoinse);
+        console.log('ERROR [%s]', body);
     };
     var tweets;
     var success = function (data) {
@@ -13,25 +15,27 @@ var Twitter = require('twitter-node-client').Twitter;
 
           console.log("The file was saved!");
           const exec = require('child_process').exec;
-          exec("R CMD BATCH '--args ./test.JSON' simpleRFile.R", (error, stdout, stderr) => {
+
+          exec("Rscript TopFiveFriends.R ./test.JSON", (error, stdout, stderr) => {
             if (error) {
               console.error(`exec error: ${error}`);
               return;
             }
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            console.log(start-Date.now());
+            console.log('stdout: ' + stdout );
+            var fiveNames = stdout.split('\n');
+            console.log('stderr: '+ stderr);
+            console.log(fiveNames);
           });
 
       });
     };
-    var config = require('./config.js');
+    var config = require('../config.js');
 
     var twitter = new Twitter(config);
 
     var start=Date.now();
-    twitter.getUserTimeline({ screen_name: 'MarkFajita', count: '10'}, error, success);
+    twitter.getUserTimeline({ screen_name: 'MarkFajita', count: '200'}, error, success);
 
 	module.exports = function(cb) {
-		twitter.getUserTimeline({ screen_name: 'MarkFajita', count: '10'}, error, cb);
+		twitter.getUserTimeline({ screen_name: 'MarkFajita', count: '200'}, error, cb);
 	}
